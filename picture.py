33362b5e210e96af2a807d2adaf7680531d82e5d
Daniel Monteagudo <dmonte3210@gmail.com>
2017-02-04 17:26:27 -0500

    read from file instead

--- picture.py
+++ picture.py
@@ -1,12 +1,14 @@
+from PIL import Image
 import math
 
-f = open("leedle.ppm", "w")
-
-text = raw_input("What to encode: ")
+read = raw_input("File to encode: ")
+rf = open(read, "r")
+text = rf.read()
+rf.close()
 width = len(text)#720
 height = len(text)#720
-
-f.write("P3 %s %s 255\n" % (width, height))
+f = open("raw.ppm", "wb")
+f.write('P3 %s %s 255\n' % (width, height))
 
 bigstr = ""
 
@@ -14,18 +16,18 @@ oy = height/2.0
 ox = width/2.0
 mdist = int(math.sqrt(((oy)**2 + (ox)**2)))
 numstr = ""
+
+
 for y in range(0, height):
     for x in range(0, width):
         dist = int(math.sqrt(((oy-y)**2 + (ox-x)**2)))
         normdist = int(255 * dist / mdist)
         r = 255 - (normdist - ord(text[x]))
-        if y == 0:
-            numstr += str(ord(text[x])) + " "
+        #if y == 0:
+        #    numstr += str(ord(text[x])) + " "
         g = 255 - (normdist - ord(text[y]))
-        b = 255 - normdist
-        bigstr += "%s %s %s\n" % (r, g, b)
-
-bigstr = bigstr[:len(bigstr)-2]
+        b = max(r, g)
+        bigstr += '%s %s %s\n' % (r, g, b)
 
 f.write(bigstr)
-    
+f.close()
