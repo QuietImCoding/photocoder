09ffc1800862452d09e2f4ac265d34ece9ab9698
Daniel Monteagudo <dmonte3210@gmail.com>
2017-02-04 22:56:39 -0500

    allowed for encoding larger files

--- decode.py
+++ decode.py
@@ -10,22 +10,41 @@ pix = im.load()
 #print "Width: " + str(im.size[0]) + ", Height: " + str(im.size[1])
 oy = height/2.0
 ox = width/2.0
-mdist = int(math.sqrt(((oy)**2 + (ox)**2)))
-numlist = []
-for x in range(0, width):
-    dist = int(math.sqrt(((oy)**2 + (ox-x)**2)))
-    normdist = int(255 * dist / mdist)
-    r = pix[x, 0][0]
-    number = r - 255 + normdist
-    numlist.append(number)
 
-for y in range(0, height):
-    dist = int(math.sqrt(((oy-y)**2 + (ox)**2)))
-    normdist = int(255 * dist / mdist)
-    g = pix[0, y][1]
-    number = g - 255 + normdist
-    numlist[y] = int(round((numlist[y] + number) / 2))
+def decode(big=False):
+    mdist = int(math.sqrt(((oy)**2 + (ox)**2)))
+    numlist = []
+    if big:
+        for i in range(1, int(width * height)-1):
+            x = int(i % width)
+            y = int(i / height)
+            dist = int(math.sqrt(((oy-y)**2 + (ox-x)**2)))
+            normdist = int(255 * dist / mdist)
+            r = pix[x, y][0]
+            num = r - 255 + normdist
+            if num >= 0 and num <= 255:
+                numlist.append(num)
+            if r == 0:
+                if pix[x, y][1] == 1 and pix[x,y][2] == 2:
+                    return numlist
+    else:
+        for x in range(0, width):
+                dist = int(math.sqrt(((oy)**2 + (ox-x)**2)))
+                normdist = int(255 * dist / mdist)
+                r = pix[x, 0][0]
+                number = r - 255 + normdist
+                if number > 255 or number < 0:
+                    number = 255
+                numlist.append(number)
+        return numlist
 
+if pix[0,0] == (0, 1, 2):
+    print "BIG"
+    numlist = decode(True)
+else:
+    print pix[0,0]
+    numlist = decode()
+    
 mystery = ""
 for i in numlist:
     mystery += chr(i)
--- picture.py
+++ picture.py
@@ -5,29 +5,69 @@ read = raw_input("File to encode: ")
 rf = open(read, "r")
 text = rf.read()
 rf.close()
-width = len(text)#720
-height = len(text)#720
-f = open("raw.ppm", "wb")
-f.write('P3 %s %s 255\n' % (width, height))
-
-bigstr = ""
-
-oy = height/2.0
-ox = width/2.0
-mdist = int(math.sqrt(((oy)**2 + (ox)**2)))
-numstr = ""
 
 
-for y in range(0, height):
-    for x in range(0, width):
+def smallencode(text):
+    width = len(text)
+    height = len(text)
+    f = open("raw.ppm", "w")
+    f.write('P3 %s %s 255\n' % (width, height))
+
+    bigstr = ""
+    
+    oy = height/2.0
+    ox = width/2.0
+    mdist = int(math.sqrt(((oy)**2 + (ox)**2)))
+    numstr = ""
+
+
+    for y in range(0, height):
+        for x in range(0, width):
+            dist = int(math.sqrt(((oy-y)**2 + (ox-x)**2)))
+            normdist = int(255 * dist / mdist)
+            r = 255 - (normdist - ord(text[x]))
+            #if y == 0:
+            #    numstr += str(ord(text[x])) + " "
+            g = 255 - (normdist - ord(text[y]))
+            b = max(r, g)
+            bigstr += '%s %s %s\n' % (r, g, b)
+
+    f.write(bigstr)
+    f.close()
+
+def bigencode(text):
+    size = math.ceil(math.sqrt(len(text)))
+    width = size
+    height = size
+    f = open("raw.ppm", "w")
+    f.write('P3 %s %s 255\n' % (int(width), int(height)))
+
+    bigstr = ""
+    
+    oy = height/2.0
+    ox = width/2.0
+    mdist = int(math.sqrt(((oy)**2 + (ox)**2)))
+    numstr = ""
+    bigstr += '0 1 2\n'
+    for i in range(1, int(size**2)-1):
+        x = int(i % width)
+        y = int(i / height)
         dist = int(math.sqrt(((oy-y)**2 + (ox-x)**2)))
         normdist = int(255 * dist / mdist)
-        r = 255 - (normdist - ord(text[x]))
-        #if y == 0:
-        #    numstr += str(ord(text[x])) + " "
-        g = 255 - (normdist - ord(text[y]))
-        b = max(r, g)
+        if i < len(text):
+            r = 255 - (normdist - ord(text[i]))
+            g = 255 - (normdist - ord(text[x]))
+            b = max(r, g)
+        else:
+            r = 0
+            g = 1
+            b = 2
         bigstr += '%s %s %s\n' % (r, g, b)
 
-f.write(bigstr)
-f.close()
+    f.write(bigstr)
+    f.close()
+
+if len(text) < 2500:
+    smallencode(text)
+else:
+    bigencode(text)
